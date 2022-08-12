670f1a315d69656b06b888a65b9bd56aefe71250
Satoshi Konno <skonno@cybergarage.org>
2022-08-13 00:24:57 +0900

    Add Message::AddQuestion()

---
+++ mdns/protocol/resource.go
@@ -0,0 +1,97 @@
+// Copyright (C) 2022 The go-mdns Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	"io"
+
+	"github.com/cybergarage/go-mdns/mdns/encoding"
+)
+
+type ResourceType = Type
+type ResourceClass = Class
+
+// Resource represents a question.
+type Resource struct {
+	Name       string
+	Type       ResourceType
+	CacheFlush bool
+	Class      ResourceClass
+}
+
+// NewResource returns a new question innstance.
+func NewResource() *Resource {
+	return &Resource{
+		Name:       "",
+		Type:       0,
+		CacheFlush: false,
+		Class:      0,
+	}
+}
+
+// NewResourceWithReader returns a new question innstance with the specified reader.
+func NewResourceWithReader(reader io.Reader) (*Resource, error) {
+	res := NewResource()
+	return res, res.Parse(reader)
+}
+
+// Parse parses the specified reader.
+func (res *Resource) Parse(reader io.Reader) error {
+	// Parses domain names
+	nextNameLenBuf := make([]byte, 1)
+	_, err := reader.Read(nextNameLenBuf)
+	for err == nil {
+		nextNameLen := encoding.BytesToInteger(nextNameLenBuf)
+		if nextNameLen == 0 {
+			break
+		}
+		nextName := make([]byte, nextNameLen)
+		_, err = reader.Read(nextName)
+		if err != nil {
+			return err
+		}
+		if 0 < len(res.Name) {
+			res.Name += "."
+		}
+		res.Name += string(nextName)
+		_, err = reader.Read(nextNameLenBuf)
+	}
+	if err != nil {
+		return err
+	}
+
+	// Parses query type
+	queryTypeBuf := make([]byte, 2)
+	_, err = reader.Read(queryTypeBuf)
+	if err != nil {
+		return err
+	}
+	res.Type = ResourceType(encoding.BytesToInteger(queryTypeBuf))
+
+	// Parses c;ass type
+	classBuf := make([]byte, 2)
+	_, err = reader.Read(classBuf)
+	if err != nil {
+		return err
+	}
+	class := encoding.BytesToInteger(classBuf)
+	res.CacheFlush = false
+	if (class & cacheFlushMask) != 0 {
+		res.CacheFlush = true
+	}
+	res.Class = ResourceClass(class & classMask)
+
+	return nil
+}
---
+++ mdns/protocol/resource_test.go
@@ -0,0 +1,54 @@
+// Copyright (C) 2022 The go-mdns Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestResource(t *testing.T) {
+	t.Run("ParseAuthoritative", func(t *testing.T) {
+		tests := []struct {
+			query         []byte
+			expectedName  string
+			expectedType  ResourceType
+			expectedCache bool
+			expectedClass ResourceClass
+		}{
+			{
+				query:         []byte{0x00, 0x00, 0x29, 0x05, 0xa0, 0x00, 0x00, 0x11, 0x94, 0x00, 0x12, 0x00, 0x04, 0x00, 0x0e, 0x00, 0x74, 0x52, 0x06, 0x8d, 0xcf, 0x54, 0x27, 0x86, 0xfd, 0xcd, 0x88, 0xe1, 0x43},
+				expectedName:  "",
+				expectedType:  OPT,
+				expectedCache: false,
+			},
+		}
+		for _, test := range tests {
+			q, err := NewResourceWithReader(bytes.NewReader(test.query))
+			if err != nil {
+				t.Error(err)
+			}
+			if q.Name != test.expectedName {
+				t.Errorf("%s != %s", q.Name, test.expectedName)
+			}
+			if q.Type != test.expectedType {
+				t.Errorf("%2X != %2X", q.Type, test.expectedType)
+			}
+			if q.CacheFlush != test.expectedCache {
+				t.Errorf("%t != %t", q.CacheFlush, test.expectedCache)
+			}
+		}
+	})
+}
