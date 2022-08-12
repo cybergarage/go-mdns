edc61f33affa276d9a5cebee710c3bba68d3019e
Satoshi Konno <skonno@cybergarage.org>
2022-08-13 00:29:54 +0900

    Update Message::Bytes()

--- mdns/protocol/message.go
+++ mdns/protocol/message.go
@@ -103,7 +103,8 @@ func (msg *Message) Copy() *Message {
 
 // Bytes returns the binary representation.
 func (msg *Message) Bytes() []byte {
-	return msg.Header.Bytes()
+	bytes := msg.Header.Bytes()
+	return bytes
 }
 
 // String returns the string representation.
