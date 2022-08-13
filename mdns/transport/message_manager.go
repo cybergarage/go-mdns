b7353d17fdd71c7c70fe54a84403f800cc21d64a
Satoshi Konno <skonno@cybergarage.org>
2022-08-13 20:12:41 +0900

    Remove overrided methods

--- mdns/transport/message_manager.go
+++ mdns/transport/message_manager.go
@@ -21,49 +21,25 @@ import (
 // A MessageManager represents a multicast server list.
 type MessageManager struct {
 	messageHandler protocol.MessageHandler
-	multicastMgr   *MulticastManager
+	*MulticastManager
 }
 
 // NewMessageManager returns a new message manager.
 func NewMessageManager() *MessageManager {
 	mgr := &MessageManager{
-		messageHandler: nil,
-		multicastMgr:   NewMulticastManager(),
+		messageHandler:   nil,
+		MulticastManager: NewMulticastManager(),
 	}
 	return mgr
 }
 
 // SetMessageHandler set a listener to all managers.
 func (mgr *MessageManager) SetMessageHandler(h protocol.MessageHandler) {
-	mgr.multicastMgr.SetHandler(h)
+	mgr.SetHandler(h)
 	mgr.messageHandler = h
 }
 
 // GetMessageHandler returns the listener of the manager.
 func (mgr *MessageManager) GetMessageHandler() protocol.MessageHandler {
 	return mgr.messageHandler
-}
-
-// Start starts all transport managers.
-func (mgr *MessageManager) Start() error {
-	err := mgr.Stop()
-	if err != nil {
-		return err
-	}
-	err = mgr.multicastMgr.Start()
-	if err != nil {
-		mgr.Stop()
-		return err
-	}
-	return nil
-}
-
-// Stop stops all transport managers.
-func (mgr *MessageManager) Stop() error {
-	return mgr.multicastMgr.Stop()
-}
-
-// IsRunning returns true whether the local managers are running, otherwise false.
-func (mgr *MessageManager) IsRunning() bool {
-	return mgr.multicastMgr.IsRunning()
 }
