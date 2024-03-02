3fc38d436ebb84eb6d0fb973aa51fe6a00ab3609
Satoshi Konno <skonno@cybergarage.org>
2024-03-03 01:30:34 +0900

    Add Records::String()

--- mdns/protocol/records.go
+++ mdns/protocol/records.go
@@ -14,5 +14,48 @@
 
 package protocol
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Records is a list of Record.
 type Records []Record
+
+// String returns the string representation.
+func (records Records) String() string {
+	type record []string
+
+	lines := []record{}
+	for _, r := range records {
+		record := record{
+			r.Name(),
+			r.Type().String(),
+			r.Content(),
+		}
+		lines = append(lines, record)
+	}
+
+	maxRecordLen := []int{0, 0, 0}
+	for _, r := range lines {
+		for n, s := range r {
+			if maxRecordLen[n] < len(s) {
+				maxRecordLen[n] = len(s)
+			}
+		}
+	}
+
+	str := ""
+	for _, r := range lines {
+		for n, s := range r {
+			sfmt := "%" + strconv.Itoa(maxRecordLen[n]) + "s"
+			str += fmt.Sprintf(sfmt, s)
+			if n < len(r)-1 {
+				str += " "
+			}
+		}
+		str += "\n"
+	}
+
+	return str
+}
