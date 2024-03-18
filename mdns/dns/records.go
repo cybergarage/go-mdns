d9e4d1f2044ed1972573a19f0cb7c12f2a41a141
Satoshi Konno <skonno@cybergarage.org>
2024-03-18 22:40:01 +0900

    Add Records::Lookup*()

--- mdns/dns/records.go
+++ mdns/dns/records.go
@@ -22,13 +22,145 @@ import (
 // Records is a list of Record.
 type Records []Record
 
-// NewRecorsWithResourceRecords returns a new Records with the specified resource records.
-func NewRecorsWithResourceRecords(resourceRecords ResourceRecords) Records {
-	records := Records{}
-	for _, resourceRecord := range resourceRecords {
-		records = append(records, resourceRecord)
+// HasRecord returns true if the resource record of the specified name is included in the list. otherwise false.
+func (records Records) HasRecord(name string) bool {
+	_, ok := records.LookupRecordForName(name)
+	return ok
+}
+
+// LookupRecordForName returns the resource record of the specified name.
+func (records Records) LookupRecordForName(name string) (Record, bool) {
+	lookupRecords := records.LookupRecordsForName(name)
+	if len(lookupRecords) == 0 {
+		return nil, false
 	}
-	return records
+	return lookupRecords[0], true
+}
+
+// LookupRecordForNamePrefix returns the resource record of the specified name prefix.
+func (records Records) LookupRecordForNamePrefix(prefix string) (Record, bool) {
+	lookupRecords := records.LookupRecordsForNamePrefix(prefix)
+	if len(lookupRecords) == 0 {
+		return nil, false
+	}
+	return lookupRecords[0], true
+}
+
+// LookupRecordForNameSuffix returns the resource record of the specified name suffix.
+func (records Records) LookupRecordForNameSuffix(suffix string) (Record, bool) {
+	lookupRecords := records.LookupRecordsForNameSuffix(suffix)
+	if len(lookupRecords) == 0 {
+		return nil, false
+	}
+	return lookupRecords[0], true
+}
+
+// LookupRecordForType returns the resource record of the specified type.
+func (records Records) LookupRecordForType(t Type) (Record, bool) {
+	lookupRecords := records.LookupRecordsForType(t)
+	if len(lookupRecords) == 0 {
+		return nil, false
+	}
+	return lookupRecords[0], true
+}
+
+// LookupRecordsForNamePrefix returns the resource records of the specified name prefix.
+func (records Records) LookupRecordsForNamePrefix(prefix string) Records {
+	lookupRecords := Records{}
+	for _, record := range records {
+		if record.HasNamePrefix(prefix) {
+			lookupRecords = append(lookupRecords, record)
+		}
+	}
+	return lookupRecords
+}
+
+// LookupRecordsForNameSuffix returns the resource records of the specified name suffix.
+func (records Records) LookupRecordsForNameSuffix(suffix string) Records {
+	lookupRecords := Records{}
+	for _, record := range records {
+		if record.HasNameSuffix(suffix) {
+			lookupRecords = append(lookupRecords, record)
+		}
+	}
+	return lookupRecords
+}
+
+// LookupRecordsForName returns the resource records of the specified name.
+func (records Records) LookupRecordsForName(name string) Records {
+	lookupRecords := Records{}
+	for _, record := range records {
+		if record.IsName(name) {
+			lookupRecords = append(lookupRecords, record)
+		}
+	}
+	return lookupRecords
+}
+
+// LookupRecordForType returns the resource records of the specified type.
+func (records Records) LookupRecordsForType(t Type) []Record {
+	resRecords := []Record{}
+	for _, record := range records {
+		if record.Type() == t {
+			resRecords = append(resRecords, record)
+		}
+	}
+	return resRecords
+}
+
+// LookupARecords returns the A records.
+func (records Records) LookupARecords() []*ARecord {
+	resRecords := []*ARecord{}
+	for _, record := range records {
+		if aRecord, ok := record.(*ARecord); ok {
+			resRecords = append(resRecords, aRecord)
+		}
+	}
+	return resRecords
+}
+
+// LookupAAAARecords returns the AAAA records.
+func (records Records) LookupAAAARecords() []*AAAARecord {
+	resRecords := []*AAAARecord{}
+	for _, record := range records {
+		if aaaaRecord, ok := record.(*AAAARecord); ok {
+			resRecords = append(resRecords, aaaaRecord)
+		}
+	}
+	return resRecords
+}
+
+// LookupPTRRecords returns the PTR records.
+func (records Records) LookupPTRRecords() []*PTRRecord {
+	resRecords := []*PTRRecord{}
+	for _, record := range records {
+		if ptrRecord, ok := record.(*PTRRecord); ok {
+			resRecords = append(resRecords, ptrRecord)
+		}
+	}
+	return resRecords
+}
+
+// LookupSRVRecords returns the SRV records.
+func (records Records) LookupSRVRecords() []*SRVRecord {
+	resRecords := []*SRVRecord{}
+	for _, record := range records {
+		if srvRecord, ok := record.(*SRVRecord); ok {
+			resRecords = append(resRecords, srvRecord)
+		}
+	}
+	return resRecords
+}
+
+// LookupTXTRecords returns the TXT records.
+func (records Records) LookupTXTRecords() []*TXTRecord {
+	resRecords := []*TXTRecord{}
+	for _, record := range records {
+		if txtRecord, ok := record.(*TXTRecord); ok {
+			resRecords = append(resRecords, txtRecord)
+		}
+	}
+	return resRecords
 }
 
 // String returns the string representation.
