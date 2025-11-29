e884971021a4af1d478fec0cba53a4deb656fd1a
Satoshi Konno <skonno@cybergarage.org>
2025-11-30 02:34:00 +0900

    test: add TestServiceMessages()

---
+++ mdnstest/service_test.go
@@ -0,0 +1,65 @@
+// Copyright (C) 2022 The go-mdns Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mdnstest
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/cybergarage/go-logger/log/hexdump"
+	"github.com/cybergarage/go-mdns/mdns"
+	"github.com/cybergarage/go-mdns/mdns/dns"
+)
+
+func TestServiceMessages(t *testing.T) {
+
+	t.Run("matter", func(t *testing.T) {
+
+		tests := []struct {
+			name   string
+			msgLog string
+		}{
+			{
+				"matter-service-01",
+				matterService01,
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				msgBytes, err := hexdump.DecodeHexdumpLogs(strings.Split(test.msgLog, "\n"))
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				msg, err := dns.NewMessageWithBytes(msgBytes)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				_, err = mdns.NewService(
+					mdns.WithServiceMessage(msg),
+				)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+			})
+		}
+	})
+}
