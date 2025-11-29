0f83006cbb1c564e27a8ae3eee9078c239d96a3b
Satoshi Konno <skonno@cybergarage.org>
2025-11-29 10:27:53 +0900

    cmd(mdnsctrl): add doc command

--- mdns/cmd/doc.go
+++ mdns/cmd/doc.go
@@ -15,7 +15,9 @@
 package cmd
 
 import (
-	"os"
+	"bytes"
+	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
@@ -25,7 +27,44 @@ var docCmd = &cobra.Command{
 	Use:   "doc",
 	Short: "Generate markdown documentation to stdout",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return doc.GenMarkdown(cmd.Root(), os.Stdout)
+		emptyLinkHandler := func(name string) string {
+			return ""
+		}
+
+		var appendMarkdown func(cmd *cobra.Command, buf *bytes.Buffer)
+		appendMarkdown = func(cmd *cobra.Command, buf *bytes.Buffer) {
+			doc.GenMarkdownCustom(cmd, buf, emptyLinkHandler)
+			for _, c := range cmd.Commands() {
+				appendMarkdown(c, buf)
+			}
+		}
+
+		removeEmptySeeAlsoSections := func(md string) string {
+			lines := strings.Split(md, "\n")
+			var out []string
+			for i := 0; i < len(lines); i++ {
+				// Check for start of a meaningless SEE ALSO section (only an empty link follows)
+				// ### SEE ALSO
+				//
+				// * [mdnsctl]()	 -
+				if i < len(lines)-2 && strings.HasPrefix(lines[i], "### SEE ALSO") {
+					// Skip these two lines as they do not provide any meaningful information
+					i += 2 // skip the next two lines
+					continue
+				}
+				out = append(out, lines[i])
+			}
+			return strings.Join(out, "\n")
+		}
+
+		var buf bytes.Buffer
+		appendMarkdown(rootCmd, &buf)
+		// ---- Post-process: Remove meaningless SEE ALSO sections ----
+		docStr := buf.String()
+		filteredDocStr := removeEmptySeeAlsoSections(docStr)
+		fmt.Print(filteredDocStr)
+
+		return nil
 	},
 }
 
