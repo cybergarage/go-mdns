6946b92860380a1b2686fae4434800bfefed1e80
Satoshi Konno <skonno@cybergarage.org>
2026-02-02 13:00:45 +0900

    feat(transport): add unicast support to message manager

--- mdns/transport/message_manager.go
+++ mdns/transport/message_manager.go
@@ -15,18 +15,22 @@
 package transport
 
 import (
+	"errors"
+
 	"github.com/cybergarage/go-mdns/mdns/dns"
 )
 
 // A MessageManager represents a multicast server list.
 type MessageManager struct {
 	*MulticastManager
+	*UnicastManager
 }
 
 // NewMessageManager returns a new message manager.
 func NewMessageManager() *MessageManager {
 	mgr := &MessageManager{
 		MulticastManager: NewMulticastManager(),
+		UnicastManager:   NewUnicastManager(),
 	}
 	return mgr
 }
@@ -34,4 +38,40 @@ func NewMessageManager() *MessageManager {
 // SetMessageProcessor sets the message processor.
 func (mgr *MessageManager) SetMessageProcessor(processor dns.MessageProcessor) {
 	mgr.MulticastManager.SetMessageProcessor(processor)
+	mgr.UnicastManager.SetMessageProcessor(processor)
+}
+
+// AnnounceMessage sends a message to the multicast address.
+func (mgr *MessageManager) AnnounceMessage(msg dns.Message) error {
+	return mgr.UnicastManager.AnnounceMessage(msg)
+}
+
+// Start starts this server.
+func (mgr *MessageManager) Start() error {
+	starter := []func() error{
+		mgr.UnicastManager.Start,
+		mgr.MulticastManager.Start,
+	}
+	var err error
+	for _, startFunc := range starter {
+		if startErr := startFunc(); startErr != nil {
+			err = errors.Join(err, startErr)
+		}
+	}
+	return err
+}
+
+// Stop stops this server.
+func (mgr *MessageManager) Stop() error {
+	stopper := []func() error{
+		mgr.MulticastManager.Stop,
+		mgr.UnicastManager.Stop,
+	}
+	var err error
+	for _, stopFunc := range stopper {
+		if stopErr := stopFunc(); stopErr != nil {
+			err = errors.Join(err, stopErr)
+		}
+	}
+	return err
 }
